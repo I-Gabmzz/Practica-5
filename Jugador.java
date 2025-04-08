ace6faa5b91b1d68d1592012ad3dbb46c409aa52
Dierik28 <diego.alfonso@uabc.edu.mx>
2025-04-07 21:44:03 -0700

    Se agregan unos métodos principales a las clases jugador y letra

--- Jugador.java
+++ Jugador.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+
 public class Jugador {
+    private String nombre;
+    private ArrayList<Letra> letras;
+    private int puntuacionRonda ;
+    private int puntuacionTotal ;
+
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+        this.letras = new ArrayList<>();
+        this.puntuacionRonda = 0;
+        this.puntuacionTotal = 0;
+    }
+
+    public int getPuntuacionRonda() {
+        return puntuacionRonda;
+    }
+    public void setPuntuacionRonda(int puntuacionRonda) {
+        this.puntuacionRonda = puntuacionRonda;
+    }
+    public int getPuntuacionTotal() {
+        return puntuacionTotal;
+    }
+    public void setPuntuacionTotal(int puntuacionTotal) {
+        this.puntuacionTotal = puntuacionTotal;
+    }
 }
--- Letra.java
+++ Letra.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class Letra {
+    private String letra;
+
+    public Letra(String letra) {
+        this.letra = letra;
+    }
+    public String getLetra() {
+        return letra;
+    }
+    public void setLetra(String letra) {
+        this.letra = letra;
+    }
+    public ArrayList<String> darLetras(String letra) {
+        ArrayList<String> letras = new ArrayList<String>();
+        letras.add(letra);
+        return letras;
+    }
 }
