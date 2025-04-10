be093f7cff26179e7b71145f8f2054c3fec8e086
PC OSTRICH <a1197503@uabc.edu.mx>
2025-04-09 23:51:21 -0700

    Se solicita el nombre del jugador en la clase Interfaz, se borro la clase letra debido a su redundancia y se le asignaron atributos a la clase MagoDePalabras

--- Interfaz.java
+++ Interfaz.java
@@ -2,15 +2,22 @@ import java.util.Scanner;
 
 public class Interfaz {
 
+    public Interfaz() {
+        Scanner scanner = new Scanner(System.in);
+        solicitarJugadores(scanner);
+        solicitarNombreDeJugador(scanner, 1);
+    }
+
     public static void main(String[] args) {
         System.out.println("\n========================");
         System.out.println("\n| MAGO DE LAS PALABRAS |\n");
         System.out.println("========================\n");
+        Interfaz interfaz = new Interfaz();
     }
 
-    public int solicitarJugadores() {
+    public int solicitarJugadores(Scanner scanner) {
         int numeroDeJugadores;
-        Scanner scanner = new Scanner(System.in);
+        scanner = new Scanner(System.in);
         while (true) {
             System.out.println("\nIngrese el numero de jugadores (2 - 4): ");
             numeroDeJugadores = scanner.nextInt();
@@ -21,4 +28,14 @@ public class Interfaz {
             }
         }
     }
+
+    public String solicitarNombreDeJugador(Scanner scanner, int numeroDeJugadores) {
+        System.out.println("\nIngrese el nombre del jugador " + numeroDeJugadores + ": ");
+        String nombre = scanner.next();
+        return nombre;
+    }
+
+
+
+
 }
