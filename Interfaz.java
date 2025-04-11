a2ffe0ad1aea1218d8710365435c5575cb8c1705
PC OSTRICH <a1197503@uabc.edu.mx>
2025-04-10 23:50:13 -0700

    En la clase Interfaz se agrego un metodo que imprimiera los resultados.

--- Interfaz.java
+++ Interfaz.java
@@ -1,47 +1,52 @@
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Interfaz {
     final String negrita = "\u001B[1m";
 
     public Interfaz() {
         Scanner scanner = new Scanner(System.in);
+        imprimirTituloDelJuego();
         solicitarJugadores(scanner);
         solicitarNombreDeJugador(scanner, 1);
         solicitarModoDeJuego(scanner);
     }
 
     public static void main(String[] args) {
+        Interfaz interfaz = new Interfaz();
+    }
+
+    public void imprimirTituloDelJuego() {
         final String reset = "\u001B[0m";
         final String MORADO = "\u001B[35m";
         final String AMARILLO = "\u001B[33m";
 
         String[] titulo = {
-            "\n    ███╗   ███╗  █████╗   ██████╗   ██████╗      ██████╗  ███████╗",
-            "    ████╗ ████║ ██╔══██╗ ██╔════╝  ██╔═══██╗     ██╔══██╗ ██╔════╝",
-            "    ██╔████╔██║ ███████║ ██║  ███╗ ██║   ██║     ██║  ██║ █████╗  ",
-            "    ██║╚██╔╝██║ ██╔══██║ ██║   ██║ ██║   ██║     ██║  ██║ ██╔══╝  ",
-            "    ██║ ╚═╝ ██║ ██║  ██║ ╚██████╔╝ ╚██████╔╝     ██████╔╝ ███████╗",
-            "    ╚═╝     ╚═╝ ╚═╝  ╚═╝  ╚═════╝   ╚═════╝      ╚═════╝  ╚══════╝",
-            "                                                          ",
-            "██████╗   █████╗  ██╗       █████╗  ██████╗  ██████╗   █████╗  ███████╗",
-            "██╔══██╗ ██╔══██╗ ██║      ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔════╝",
-            "██████╔╝ ███████║ ██║      ███████║ ██████╔╝ ██████╔╝ ███████║ ███████╗",
-            "██╔═══╝  ██╔══██║ ██║      ██╔══██║ ██╔══██╗ ██╔══██╗ ██╔══██║ ╚════██║",
-            "██║      ██║  ██║ ███████╗ ██║  ██║ ██████╔╝ ██║  ██║ ██║  ██║ ███████║",
-            "╚═╝      ╚═╝  ╚═╝ ╚══════╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚══════╝"
+                "\n    ███╗   ███╗  █████╗   ██████╗   ██████╗      ██████╗  ███████╗",
+                "    ████╗ ████║ ██╔══██╗ ██╔════╝  ██╔═══██╗     ██╔══██╗ ██╔════╝",
+                "    ██╔████╔██║ ███████║ ██║  ███╗ ██║   ██║     ██║  ██║ █████╗  ",
+                "    ██║╚██╔╝██║ ██╔══██║ ██║   ██║ ██║   ██║     ██║  ██║ ██╔══╝  ",
+                "    ██║ ╚═╝ ██║ ██║  ██║ ╚██████╔╝ ╚██████╔╝     ██████╔╝ ███████╗",
+                "    ╚═╝     ╚═╝ ╚═╝  ╚═╝  ╚═════╝   ╚═════╝      ╚═════╝  ╚══════╝",
+                "                                                          ",
+                "██████╗   █████╗  ██╗       █████╗  ██████╗  ██████╗   █████╗  ███████╗",
+                "██╔══██╗ ██╔══██╗ ██║      ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔════╝",
+                "██████╔╝ ███████║ ██║      ███████║ ██████╔╝ ██████╔╝ ███████║ ███████╗",
+                "██╔═══╝  ██╔══██║ ██║      ██╔══██║ ██╔══██╗ ██╔══██╗ ██╔══██║ ╚════██║",
+                "██║      ██║  ██║ ███████╗ ██║  ██║ ██████╔╝ ██║  ██║ ██║  ██║ ███████║",
+                "╚═╝      ╚═╝  ╚═╝ ╚══════╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚══════╝"
         };
 
-            for (int i = 0; i < 6; i++) {
-                System.out.println(MORADO + titulo[i] + reset);
-            }
+        for (int i = 0; i < 6; i++) {
+            System.out.println(MORADO + titulo[i] + reset);
+        }
 
-            System.out.println(titulo[6]);
+        System.out.println(titulo[6]);
 
-            for (int i = 7; i < titulo.length; i++) {
-                System.out.println(AMARILLO + titulo[i] + reset);
-            }
-        Interfaz interfaz = new Interfaz();
+        for (int i = 7; i < titulo.length; i++) {
+            System.out.println(AMARILLO + titulo[i] + reset);
+        }
     }
 
     public int solicitarJugadores(Scanner scanner) {
@@ -89,18 +94,38 @@ public class Interfaz {
         return false;
     }
 
-    public void indicarTurnoDeJugador(Jugador player) {
+    public void indicarTurnoDeJugador(Jugador jugador) {
         final String reset = "\u001B[0m";
-        System.out.print(negrita + "\n" + player.getNombre() + " (" + player.getPuntuacionTotal() + " puntos) > " + reset);
+
+        System.out.print(negrita + "\n" + jugador.getNombre() + " (" + jugador.getPuntuacionTotal() + " puntos) > " + reset);
     }
 
-    public void imprimirRonda (int ronda, Set<Character> letras) {
+    public void imprimirRonda (int ronda, HashSet<Character> letras) {
         System.out.println("\n |===| RONDA  " + ronda + " |===|");
         System.out.println("\n\nForma palabras con las siguientes letras: " + letras);
         System.out.print("\n * Escribe una palabra usando solo estas letras o presiona Enter para pasar tu turno.");
     }
 
+    public void imprimirResultados (List<Jugador> jugadores) {
+        final String negrita_Verde = "\u001B[1;32m";
+        final String reset = "\u001B[0m";
+
+        System.out.println(negrita + "\n| === | RESULTADOS | === |" + reset);
+        System.out.println("  Tabla de Puntuaciones  ");
+
+        jugadores.sort((jugador1, jugador2) -> jugador2.getPuntuacionTotal() - jugador1.getPuntuacionTotal());
+        jugadores.forEach(jugador -> System.out.println(" - " + jugador.getNombre() + ": " + jugador.getPuntuacionTotal() + " puntos"));
+
+        if (!jugadores.isEmpty()) {
+            System.out.println(negrita_Verde + "\n¡El ganador es " + jugadores.get(0).getNombre() + "!" + reset);
+        } else {
+            System.out.println("No se puede determinar el ganador.");
+        }
+
+        System.out.println(negrita + "\nPalabras usadas por el ganador:" + reset);
+        jugadores.get(0).getPalabrasUsadas().forEach(palabras -> System.out.println(" - " + palabras));
 
 
+    }
 
 }
