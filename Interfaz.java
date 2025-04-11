9745c40312623f3c7a3593730d43bf4fa2ee7d32
PC OSTRICH <a1197503@uabc.edu.mx>
2025-04-10 22:36:44 -0700

    Se agrego un Main de mejor impresion, se aplico el uso de colores y negritas y se agregaron metodos en la clase Interfaz

--- Interfaz.java
+++ Interfaz.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class Interfaz {
+    final String negrita = "\u001B[1m";
 
     public Interfaz() {
         Scanner scanner = new Scanner(System.in);
@@ -11,17 +12,45 @@ public class Interfaz {
     }
 
     public static void main(String[] args) {
-        System.out.println("\n        ========================");
-        System.out.println("\n        | MAGO DE LAS PALABRAS |\n");
-        System.out.println("        ========================\n");
+        final String reset = "\u001B[0m";
+        final String MORADO = "\u001B[35m";
+        final String AMARILLO = "\u001B[33m";
+
+        String[] titulo = {
+            "\n    ███╗   ███╗  █████╗   ██████╗   ██████╗      ██████╗  ███████╗",
+            "    ████╗ ████║ ██╔══██╗ ██╔════╝  ██╔═══██╗     ██╔══██╗ ██╔════╝",
+            "    ██╔████╔██║ ███████║ ██║  ███╗ ██║   ██║     ██║  ██║ █████╗  ",
+            "    ██║╚██╔╝██║ ██╔══██║ ██║   ██║ ██║   ██║     ██║  ██║ ██╔══╝  ",
+            "    ██║ ╚═╝ ██║ ██║  ██║ ╚██████╔╝ ╚██████╔╝     ██████╔╝ ███████╗",
+            "    ╚═╝     ╚═╝ ╚═╝  ╚═╝  ╚═════╝   ╚═════╝      ╚═════╝  ╚══════╝",
+            "                                                          ",
+            "██████╗   █████╗  ██╗       █████╗  ██████╗  ██████╗   █████╗  ███████╗",
+            "██╔══██╗ ██╔══██╗ ██║      ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔══██╗ ██╔════╝",
+            "██████╔╝ ███████║ ██║      ███████║ ██████╔╝ ██████╔╝ ███████║ ███████╗",
+            "██╔═══╝  ██╔══██║ ██║      ██╔══██║ ██╔══██╗ ██╔══██╗ ██╔══██║ ╚════██║",
+            "██║      ██║  ██║ ███████╗ ██║  ██║ ██████╔╝ ██║  ██║ ██║  ██║ ███████║",
+            "╚═╝      ╚═╝  ╚═╝ ╚══════╝ ╚═╝  ╚═╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝  ╚═╝ ╚══════╝"
+        };
+
+            for (int i = 0; i < 6; i++) {
+                System.out.println(MORADO + titulo[i] + reset);
+            }
+
+            System.out.println(titulo[6]);
+
+            for (int i = 7; i < titulo.length; i++) {
+                System.out.println(AMARILLO + titulo[i] + reset);
+            }
         Interfaz interfaz = new Interfaz();
     }
 
     public int solicitarJugadores(Scanner scanner) {
         int numeroDeJugadores;
         scanner = new Scanner(System.in);
+        final String reset = "\u001B[0m";
+
         while (true) {
-            System.out.print("\n * Ingrese el numero de jugadores (2 - 4): ");
+            System.out.print(negrita + "\n * Ingrese el numero de jugadores (2 - 4): " + reset);
             numeroDeJugadores = scanner.nextInt();
             if (numeroDeJugadores >= 2 && numeroDeJugadores <= 4) {
                 return numeroDeJugadores;
@@ -32,31 +61,46 @@ public class Interfaz {
     }
 
     public String solicitarNombreDeJugador(Scanner scanner, int numeroDeJugadores) {
-        System.out.print("\n * Ingrese el nombre del jugador " + numeroDeJugadores + ": ");
+        final String reset = "\u001B[0m";
+
+        System.out.print(negrita + "\n * Ingrese el nombre del jugador " + numeroDeJugadores + ": " + reset);
         String nombre = scanner.next();
         return nombre;
     }
 
     public boolean solicitarModoDeJuego(Scanner scanner) {
         int opcModoDeJuego;
+        final String verde = "\u001B[32m";
+        final String rojo = "\u001B[31m";
+        final String reset = "\u001B[0m";
 
         while (true) {
-            System.out.println("\n * Ingrese el numero del modo de juego: ");
-            System.out.println("\n  1 -> Normal");
-            System.out.println("  2 -> Experto\n");
+            System.out.println("\n |===| " + negrita + "Modos de Juego" + reset + " |===| ");
+            System.out.println("\n  1 -> " + verde + "Normal" + reset);
+            System.out.println("  2 -> " + rojo + "Experto" + reset + "\n");
+            System.out.print(" * Ingrese el numero del modo de juego deseado: ");
             opcModoDeJuego = scanner.nextInt();
             if (opcModoDeJuego == 1 || opcModoDeJuego == 2) {
                 break;
             } else {
-                System.out.println("Modo fuera del rango, unicamente ingresa 1 o 2.");
+                System.out.println("La entrada esta fuera del rango, unicamente ingresa 1 o 2.");
             }
         }
         return false;
     }
 
-    public void indicarTurnoDeJugador(Jugador player, Set<Character> letras) {
-        System.out.print("\n" + player.getNombre() + " (" + player.getPuntuacionTotal() + " puntos) > ");
+    public void indicarTurnoDeJugador(Jugador player) {
+        final String reset = "\u001B[0m";
+        System.out.print(negrita + "\n" + player.getNombre() + " (" + player.getPuntuacionTotal() + " puntos) > " + reset);
     }
+
+    public void imprimirRonda (int ronda, Set<Character> letras) {
+        System.out.println("\n |===| RONDA  " + ronda + " |===|");
+        System.out.println("\n\nForma palabras con las siguientes letras: " + letras);
+        System.out.print("\n * Escribe una palabra usando solo estas letras o presiona Enter para pasar tu turno.");
+    }
+
+
 
 
 }
